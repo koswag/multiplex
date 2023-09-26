ebb9042569797c0c67b0d35bb3e8c1faedb15417
kskarzynski <k.skarzynski@sages.pl>
2023-09-26 17:13:20 +0200

    User info validation errors

---
+++ src/main/kotlin/pl/kskarzynski/multiplex/domain/user/validation/UserInfoValidationError.kt
@@ -0,0 +1,11 @@
+package pl.kskarzynski.multiplex.domain.user.validation
+
+sealed interface UserInfoValidationError {
+    data object NameTooShort : UserInfoValidationError
+    data object NameNotCapitalized : UserInfoValidationError
+
+    data object SurnameTooShort : UserInfoValidationError
+    data object SurnameNotCapitalized : UserInfoValidationError
+    data object SurnameHasTooManyHyphens : UserInfoValidationError
+    data object SurnameSecondPartIsNotCapitalized : UserInfoValidationError
+}
