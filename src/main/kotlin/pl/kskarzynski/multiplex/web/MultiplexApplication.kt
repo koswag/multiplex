fec088e61f6c812672f02874e82871cbc77c683d
kskarzynski <k.skarzynski@sages.pl>
2023-09-26 17:12:54 +0200

    Move Spring application entry point to `web` package

--- src/main/kotlin/pl/kskarzynski/multiplex/web/MultiplexApplication.kt
+++ src/main/kotlin/pl/kskarzynski/multiplex/web/MultiplexApplication.kt
@@ -1,4 +1,4 @@
-package pl.kskarzynski.multiplex
+package pl.kskarzynski.multiplex.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
