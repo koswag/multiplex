f88604d2da0ea52270134407ad38a747a1c2d3b2
kskarzynski <k.skarzynski@sages.pl>
2023-10-02 16:20:07 +0200

    Add screening ID property

--- src/main/kotlin/pl/kskarzynski/multiplex/domain/model/screening/ScreeningInfo.kt
+++ src/main/kotlin/pl/kskarzynski/multiplex/domain/model/screening/ScreeningInfo.kt
@@ -1,11 +1,16 @@
 package pl.kskarzynski.multiplex.domain.model.screening
 
 import java.time.LocalDateTime
+import java.util.UUID
+
+@JvmInline
+value class ScreeningId(val value: UUID)
 
 @JvmInline
 value class MovieTitle(val value: String)
 
 data class ScreeningInfo(
+    val id: ScreeningId,
     val title: MovieTitle,
     val time: LocalDateTime,
     val room: Room,
--- src/main/kotlin/pl/kskarzynski/multiplex/domain/model/screening/ScreeningListItem.kt
+++ src/main/kotlin/pl/kskarzynski/multiplex/domain/model/screening/ScreeningListItem.kt
@@ -3,6 +3,7 @@ package pl.kskarzynski.multiplex.domain.model.screening
 import java.time.LocalDateTime
 
 data class ScreeningListItem(
+    val id: ScreeningId,
     val title: MovieTitle,
     val time: LocalDateTime,
 )
