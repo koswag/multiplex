ccfa2558ffde1ab7e8ef2d0d7de5f0431cebc49f
kskarzynski <k.skarzynski@sages.pl>
2023-10-02 16:20:19 +0200

    Screening by ID query

--- src/main/kotlin/pl/kskarzynski/multiplex/domain/queries/ScreeningQueries.kt
+++ src/main/kotlin/pl/kskarzynski/multiplex/domain/queries/ScreeningQueries.kt
@@ -1,8 +1,11 @@
 package pl.kskarzynski.multiplex.domain.queries
 
+import pl.kskarzynski.multiplex.domain.model.screening.ScreeningId
+import pl.kskarzynski.multiplex.domain.model.screening.ScreeningInfo
 import pl.kskarzynski.multiplex.domain.model.screening.ScreeningListItem
 import java.time.LocalDateTime
 
 interface ScreeningQueries {
     fun findScreeningsByTimeSortedByTitleAndTime(time: LocalDateTime): Collection<ScreeningListItem>
+    fun findScreeningInfo(id: ScreeningId): ScreeningInfo?
 }
