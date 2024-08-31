0576a91128182a6d6f491a6b629c5850a5706bb9
kskarzynski <k.skarzynski@sages.pl>
2024-08-31 22:39:06 +0200

    refactor: Move booking ID to a separate file

---
+++ src/main/kotlin/pl/kskarzynski/multiplex/domain/model/booking/BookingId.kt
@@ -0,0 +1,11 @@
+package pl.kskarzynski.multiplex.domain.model.booking
+
+import java.util.UUID
+
+@JvmInline
+value class BookingId(val value: UUID) {
+    companion object {
+        fun generate() =
+            BookingId(UUID.randomUUID())
+    }
+}
