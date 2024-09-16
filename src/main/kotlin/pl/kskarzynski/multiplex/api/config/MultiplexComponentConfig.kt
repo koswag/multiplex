3264de1a23f69ccfc466f06348851143f8cdd3e0
kskarzynski <k.skarzynski@sages.pl>
2024-09-16 22:36:33 +0200

    feat: Register domain beans

--- src/main/kotlin/pl/kskarzynski/multiplex/api/config/MultiplexComponentConfig.kt
+++ src/main/kotlin/pl/kskarzynski/multiplex/api/config/MultiplexComponentConfig.kt
@@ -3,10 +3,27 @@ package pl.kskarzynski.multiplex.api.config
 import java.time.Clock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import pl.kskarzynski.multiplex.domain.policy.BookingExpirationPolicy
+import pl.kskarzynski.multiplex.domain.policy.BookingPricingPolicy
+import pl.kskarzynski.multiplex.domain.repo.BookingRepository
+import pl.kskarzynski.multiplex.domain.usecase.BookScreeningUseCase
 
 @Configuration
 class MultiplexComponentConfig {
 
     @Bean
     fun clock(): Clock = Clock.systemUTC()
+
+    @Bean
+    fun bookScreeningUseCase(
+        bookingPricingPolicy: BookingPricingPolicy,
+        bookingExpirationPolicy: BookingExpirationPolicy,
+        bookingRepository: BookingRepository,
+    ) = BookScreeningUseCase(bookingPricingPolicy, bookingExpirationPolicy, bookingRepository)
+
+    @Bean
+    fun bookingPricingPolicy() = BookingPricingPolicy()
+
+    @Bean
+    fun bookingExpirationPolicy() = BookingExpirationPolicy()
 }
